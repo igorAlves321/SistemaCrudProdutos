c99eb4bc26f9aec6aede7f090ca78a54434eb390
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-06-07 20:50:52 -0300

    feito o crud.

--- Produto.cs
+++ Produto.cs
@@ -2,6 +2,81 @@
 
 public class Produto
 {
+    private static List<Produto> produtos = new List<Produto>();
+    public int CodigoProduto { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
 
+    public Produto(int codigoProduto, string nome, decimal preco)
+    {
+        CodigoProduto = codigoProduto;
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public void AdicionarProduto()
+    {
+        Console.Write("Digite o código do produto: ");
+        int codigo = int.Parse(Console.ReadLine());
+        Console.Write("Digite o nome do produto: ");
+        string nome = Console.ReadLine();
+        Console.Write("Digite o preço do produto: ");
+        decimal preco = decimal.Parse(Console.ReadLine());
+        Produto produto = new Produto(codigo, nome, preco);
+        produtos.Add(produto);
+        Console.WriteLine("Produto cadastrado com exito.");
+    }
+
+    public void ListarProdutos()
+    {
+        Console.WriteLine("Lista de Produtos:");
+        foreach (var produto in produtos)
+        {
+            Console.WriteLine(produto);
+        }
+    }
+
+    public void EditarProduto()
+    {
+        Console.Write("Digite o código do produto que deseja editar: ");
+        int codigo = int.Parse(Console.ReadLine());
+        Produto produto = produtos.Find(p => p.CodigoProduto == codigo);
+
+        if (produto != null)
+        {
+            Console.Write("Digite o novo nome do produto: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Digite o novo preço do produto: ");
+            produto.Preco = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Produto editado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Produto não encontrado.");
+        }
+    }
+
+    public void ExcluirProduto()
+    {
+        Console.Write("Digite o código do produto que deseja excluir: ");
+        int codigo = int.Parse(Console.ReadLine());
+        Produto produto = produtos.Find(p => p.CodigoProduto == codigo);
+
+        if (produto != null)
+        {
+            produtos.Remove(produto);
+            Console.WriteLine("Produto excluído com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Produto não encontrado.");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Código: {CodigoProduto}, Nome: {Nome}, Preço: {Preco:C}";
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SistemaCrudProdutos;
+
+Produto produto = new Produto(0, "", 0);
+
+while (true)
+{
+    Console.WriteLine("Sistema de Cadastro de Produtos");
+    Console.WriteLine("1. Adicionar Produto");
+    Console.WriteLine("2. Listar Produtos");
+    Console.WriteLine("3. Editar Produto");
+    Console.WriteLine("4. Excluir Produto");
+    Console.WriteLine("5. Sair");
+    Console.Write("Escolha uma opção: ");
+    int opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            produto.AdicionarProduto();
+            break;
+        case 2:
+            produto.ListarProdutos();
+            break;
+        case 3:
+            produto.EditarProduto();
+            break;
+        case 4:
+            produto.ExcluirProduto();
+            break;
+        case 5:
+            return;
+        default:
+            Console.WriteLine("Opção inválida, tente novamente.");
+            break;
+    }
+}
